b600fdcab43ef454eb220ea1a8c2e487c2c7d98e
Taras Hopka <gigo123@gmail.com>
2019-05-22 14:34:43 +0300

    adapter to  INPUT KEYBOARD EVENTS fire stage  (do noting)

---
+++ src/main/java/com/thsoft/catgame/game/FireInputActionWorker.java
@@ -0,0 +1,29 @@
+package com.thsoft.catgame.game;
+
+public class FireInputActionWorker implements InputActionWorker {
+
+
+@Override
+public void LeftKey() {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void RightKey() {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void UpKey() {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void DownKey() {
+	// TODO Auto-generated method stub
+	
+}
+}
