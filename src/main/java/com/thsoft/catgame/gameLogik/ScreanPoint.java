c3e258d37cff668901de0c565e7607f07444a88c
Taras Hopka <gigo123@gmail.com>
2019-05-22 09:40:32 +0300

    new object to incupsulate  X Y  coordiates

---
+++ src/main/java/com/thsoft/catgame/gameLogik/ScreanPoint.java
@@ -0,0 +1,25 @@
+package com.thsoft.catgame.gameLogik;
+
+public class ScreanPoint {
+	private float pointX;
+	private float pointY;
+	public ScreanPoint(float pointX, float pointY) {
+		super();
+		this.pointX = pointX;
+		this.pointY = pointY;
+	}
+	public float getPointX() {
+		return pointX;
+	}
+	public void setPointX(float pointX) {
+		this.pointX = pointX;
+	}
+	public float getPointY() {
+		return pointY;
+	}
+	public void setPointY(float pointY) {
+		this.pointY = pointY;
+	}
+	
+
+}
