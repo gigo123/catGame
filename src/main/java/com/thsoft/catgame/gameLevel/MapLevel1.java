d7c4d41fe9e6ec2089c20ebd4fd094bf8e4efa32
gigo123 <gigo123black@gmail.com>
2019-06-09 11:51:00 +0300

    new consrtutor, metod to create cats, metods to L and spare key

---
+++ src/main/java/com/thsoft/catgame/gameLevel/MapLevel1.java
@@ -0,0 +1,110 @@
+package com.thsoft.catgame.gameLevel;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.thsoft.catgame.game.BaseActor;
+import com.thsoft.catgame.game.BaseScreen;
+import com.thsoft.catgame.game.InputActionWorker;
+import com.thsoft.catgame.game.MoveIputActionWorker;
+import com.thsoft.catgame.game.TilemapActor;
+import com.thsoft.catgame.gameLogik.CatMapLevel;
+import com.thsoft.catgame.gameLogik.OldMen;
+import com.thsoft.catgame.gameLogik.SolidActor;
+
+public class MapLevel1 extends BaseScreen {
+	private MapLevelVaribles mapLevelVaribles;
+	private InputActionWorker iputActionWork;
+	private MapLevelLogik mapLevelLogik;
+
+	public MapLevel1(String mapFile) {
+		super();
+		mapLevelVaribles = new MapLevelVaribles();
+		mapLevelVaribles.setMainStage(mainStage);
+		createTilemapActor(mapFile);
+		mapLevelVaribles.setWorldWidth(BaseActor.getWorldBounds().getWidth());
+		mapLevelLogik = new MapLevelLogik(mapLevelVaribles);
+		mapLevelVaribles.setMapLevelLogik(mapLevelLogik);
+		mapLevelVaribles.setLevelStage(LevelState.MOVING);
+
+	}
+
+	private void createTilemapActor(String sourseFile) {
+		Stage mainStage = mapLevelVaribles.getMainStage();
+		TilemapActor tma = new TilemapActor(sourseFile, mainStage);
+
+		for (MapObject obj : tma.getRectangleList("SolidActor")) {
+			MapProperties props = obj.getProperties();
+			new SolidActor((float) props.get("x"), (float) props.get("y"), (float) props.get("width"),
+					(float) props.get("height"), mainStage);
+
+		}
+
+		MapObject startPoint = tma.getRectangleList("start").get(0);
+		MapProperties startProps = startPoint.getProperties();
+		mapLevelVaribles
+				.setMainCharacter(new OldMen((float) startProps.get("x"), (float) startProps.get("y"), mainStage));
+
+		for (MapObject obj : tma.getRectangleList("cat1")) {
+			MapProperties props = obj.getProperties();
+			new CatMapLevel((float) props.get("x"), (float) props.get("y"), (float) props.get("width"),
+					(float) props.get("height"), mainStage);
+
+		}
+
+		mapLevelVaribles.setIputActionWork(new MoveIputActionWorker(mapLevelVaribles));
+
+	}
+
+	@Override
+	public void update(float dt) {
+		iputActionWork = mapLevelVaribles.getIputActionWork();
+		keyPressedSwich();
+		mapLevelLogik.update();
+	}
+
+	public boolean keyDown(int keyCode) {
+		// if (levelStage == LevelState.GAMEOVER)
+		// return false;
+
+		if (keyCode == Keys.L) {
+			iputActionWork.Lkey();
+
+			//mapLevelLogik.swichLevelMode();
+			return false;
+		}
+		if (keyCode == Keys.SPACE) {
+			if (mapLevelVaribles.getLevelStage() == LevelState.TARGETING) {
+				mapLevelVaribles.setLevelStage(LevelState.FIREING);
+			//	mapLevelLogik.swichLevelMode();
+			}
+		}
+		return false;
+	}
+
+	private void keyPressedSwich() {
+
+		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+			iputActionWork.LeftKey();
+		}
+		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			iputActionWork.RightKey();
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			iputActionWork.UpKey();
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			iputActionWork.DownKey();
+		}
+	}
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
